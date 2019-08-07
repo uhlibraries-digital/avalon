36f28ecdee9a2f192f613c39b69c4264e3b4ab46
Sean Watkins <slwatkins@uh.edu>
2019-08-07 14:04:01 -0500

    Update ASpace File Versions for access arks

--- app/jobs/insert_aspace_digital_object_job.rb
+++ app/jobs/insert_aspace_digital_object_job.rb
@@ -41,47 +41,68 @@ module InsertAspaceDigitalObjectJob
       @media_object = MediaObject.find(media_object_id)
       aspace_uri = self.aspace_uri
       ark_url = self.identifier
+      do_uuid = self.do_uuid
 
-      if !aspace_uri.blank? and !ark_url.blank?
-        @archival_object = InsertAspaceDigitalObjectJob::ArchivalObject.find(aspace_uri)
-        repository_id = /\/repositories\/(\d+)/.match(@archival_object.repository.ref)[1]
-        if !has_do_ark ark_url
-          Rails.logger.info "Adding Digital Object to #{aspace_uri}"
-          new_do = new_aspace_do ark_url
-          @digital_object = InsertAspaceDigitalObjectJob::DigitalObject.create(
-            repo_id: repository_id,
-            digital_object: new_do)
-          new_instance = new_aspace_instance @digital_object.uri
-          @archival_object.instances << new_instance
-          @archival_object.id = @archival_object.uri
-          @archival_object.save
-
-          # Need Avalon to house the full ASpace URL
-          ids = @media_object.other_identifier ||= []
-          ids.each.with_index do |id, index|
-            if id[:source] == 'aspace uri'
-              ids[index][:id] = InsertAspaceDigitalObjectJob::Configuration.lookup('frontend') + aspace_uri
-            end
-          end
-          @media_object.other_identifier = ids
-          @media_object.save( validate: false )
-        else
-          Rails.logger.info "Digtal object for #{ark_url} already exists"
-        end
-      else
-        Rails.logger.info "Couldn't find aspace uri and/or ark uri, I won't be doing anything"
+      if aspace_uri.blank? or ark_url.blank? or do_uuid.blank?
+        Rails.logger.info "Couldn't find aspace uri or ark uri or uuid, I won't be doing anything"
+        return
       end
+
+      @archival_object = InsertAspaceDigitalObjectJob::ArchivalObject.find(aspace_uri)
+      repository_id = /\/repositories\/(\d+)/.match(@archival_object.repository.ref)[1]
+
+      digital_object = get_digital_object do_uuid
+      
+      if digital_object.nil?
+        Rails.logger.info "Adding Digital Object to #{aspace_uri}"
+        digital_object = new_aspace_do do_uuid
+        digital_object.file_versions << new_aspace_file_version ark_url
+        @digital_object = InsertAspaceDigitalObjectJob::DigitalObject.create(
+          repo_id: repository_id,
+          digital_object: digital_object)
+        new_instance = new_aspace_instance @digital_object.uri
+        @archival_object.instances << new_instance
+        @archival_object.id = @archival_object.uri
+        @archival_object.save
+      elsif !has_do_ark digital_object.file_versions ark_url
+        @digital_object = InsertAspaceDigitalObjectJob::DigitalObject.find(digital_object.uri)
+        @digital_object.file_versions << new_aspace_file_version ark_url
+        @digital_object.id = @digital_object.uri
+        @digital_object.save
+      else
+        Rails.logger.info "Digtal object for #{ark_url} already exists"
+        return
+      end
+
+      # Need Avalon to house the full ASpace URL
+      ids = @media_object.other_identifier ||= []
+      ids.each.with_index do |id, index|
+        if id[:source] == 'aspace uri'
+          ids[index][:id] = InsertAspaceDigitalObjectJob::Configuration.lookup('frontend') + aspace_uri
+        end
+      end
+      @media_object.other_identifier = ids
+      @media_object.save( validate: false )
+        
+      
     end
 
-    def has_do_ark(ark_url)
+    def get_digital_object(uuid)
       @archival_object.instances.each do |instance|
         if instance.instance_type == "digital_object"
           digital_object = InsertAspaceDigitalObjectJob::DigitalObject.find(instance.digital_object.ref)
-          digital_object.file_versions.each do |file|
-            if file.file_uri == ark_url
-              return true
-            end
+          if digital_object.digital_object_id == uuid
+            return digital_object
           end
+        end
+      end
+      return nil
+    end
+
+    def has_do_ark(file_versions, ark_url)
+      file_versions.each do |file|
+        if file.file_uri == ark_url
+          return true
         end
       end
       return false
@@ -107,10 +128,20 @@ module InsertAspaceDigitalObjectJob
       return ''
     end
 
-    def new_aspace_do(ark_url)
+    def do_uuid
+      ids = @media_object.other_identifier ||= []
+      ids.each do |i|
+        if i[:source] == 'douuid'
+          return i[:id]
+        end
+      end
+      return ''
+    end
+
+    def new_aspace_do(uuid)
       return {
         "jsonmodel_type": "digital_object",
-        "digital_object_id": SecureRandom.uuid,
+        "digital_object_id": uuid,
         "external_ids": [],
         "subjects": [],
         "linked_events": [],
@@ -119,25 +150,13 @@ module InsertAspaceDigitalObjectJob
         "external_documents": [],
         "rights_statements": [],
         "linked_agents": [],
-        "file_versions": [
-          {
-            "jsonmodel_type": "file_version",
-            "is_representative": false,
-            "file_uri": ark_url,
-            "use_statement": "",
-            "xlink_actuate_attribute": "",
-            "xlink_show_attribute": "",
-            "file_format_name": "",
-            "file_format_version": "",
-            "checksum": "",
-            "checksum_method": ""
-          }
-        ],
+        "file_versions": [],
         "restrictions": false,
         "notes": [],
         "linked_instances": [],
-        "title": @media_object.title.presence + " (Access)",
-        "language": ""
+        "title": @media_object.title.presence,
+        "language": "",
+        "publish": true
       }
     end
 
@@ -151,6 +170,22 @@ module InsertAspaceDigitalObjectJob
         "is_representative": false
       }
     end
+  end
+
+  def new_aspace_file_version(ark_url)
+    return {
+      "jsonmodel_type": "file_version",
+      "is_representative": false,
+      "file_uri": ark_url,
+      "use_statement": "Access",
+      "xlink_actuate_attribute": "",
+      "xlink_show_attribute": "",
+      "file_format_name": "",
+      "file_format_version": "",
+      "checksum": "",
+      "checksum_method": "",
+      "publish": true
+    }
   end
 
   class ArchivalObject < Flexirest::Base
@@ -192,6 +227,7 @@ module InsertAspaceDigitalObjectJob
     before_request :replace_body
 
     get :find, ":id"
+    post :save, ":id"
     post :create, "/repositories/:repo_id/digital_objects"
 
     def add_authentication_details(name, request)
@@ -207,6 +243,9 @@ module InsertAspaceDigitalObjectJob
     def replace_body(name, request)
       if name == :create
         request.body = request.post_params[:digital_object].to_json
+      end
+      if name == :save
+        request.body = request.post_params.to_json
       end
     end
   end
