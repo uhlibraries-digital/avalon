ecb3acbbff74549c7efbb638dbe958562c4e23f0
Sean Watkins <seanlw@gmail.com>
2021-08-16 09:13:43 -0500

    Update docker-compose.yml

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ bundle check || bundle install
 
 rm -f $APP_PATH/tmp/pids/server.pid
 
-BACKGROUND=yes bundle exec rake resque:scheduler & >> $APP_PATH/log/resque_scheduler.log 2>&1
-BACKGROUND=yes QUEUE=* bundle exec rake resque:work & >> $APP_PATH/log/resque_worker.log 2>&1
+BACKGROUND=yes bundle exec rake resque:scheduler &
+BACKGROUND=yes QUEUE=* bundle exec rake resque:work &
 
 bundle exec ${@}
